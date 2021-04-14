035931f84a9b2cfa49e95601f8a7d2d2424875fd
jonbul <jonander.echeverria@gmail.com>
2021-04-15 00:23:21 +0200

    requestAnimationFrame fixed

--- public/js/canvas/game.js
+++ public/js/canvas/game.js
@@ -138,7 +138,8 @@ class Game {
         })
     }
     beginInterval() {
-        setInterval(this.intervalMethod.bind(this), 1000 / 60);
+        //setInterval(this.intervalMethod.bind(this), 1000 / 60);
+        requestAnimationFrame(this.intervalMethod.bind(this))
     }
     intervalMethod() {
 
@@ -174,7 +175,8 @@ class Game {
         }
         
         this.loadRadar();
-        requestAnimationFrame(this.drawAll.bind(this));
+        this.drawAll();
+        requestAnimationFrame(this.intervalMethod.bind(this))
     }
     clear() {
         this.context.clearRect(this.player.x - this.canvas.width, this.player.y - this.canvas.height, this.canvas.width * 2, this.canvas.height * 2);
