b804f83ea8693b09ea8faa0766ae7cf84e6109b0
Jon Ander <jonander.echeverria@gmail.com>
2025-10-07 20:06:00 +0200

    laersManager

--- public/js/paintingBoard2/layerManager.js
+++ public/js/paintingBoard2/layerManager.js
@@ -45,30 +45,29 @@ class LayerManager {
         tools.classList.add("layersManager_layer_tools")
         layerHead.appendChild(tools);
 
-        /*const layerUpBtn = document.createElement("button");
-        layerUpBtn.classList.add("btnLayerUp")
-        layerUpBtn.innerHTML = "&#708;"
-        tools.appendChild(layerUpBtn);
-        const layerDownBtn = document.createElement("button");
-        layerDownBtn.classList.add("btnLayerDown")
-        layerDownBtn.innerHTML = "&#709;";
-        tools.appendChild(layerDownBtn);*/
-
+        // layer visibility toggle button
         const layerHideBtn = document.createElement("button");
         layerHideBtn.classList.add("btnLayerHide")
         layerHideBtn.innerHTML = "&#9215;";
-        layerHideBtn.style.color = "#000"
         tools.appendChild(layerHideBtn);
+        layerHideBtn.addEventListener('click', layerToggleVisible.bind(null, layer, layerHideBtn))
+        if (!layer.visible) layerHideBtn.setAttribute("hide", "true")
 
+        // layer rename button
         const layerRename = document.createElement("button");
         layerRename.classList.add("btnLayerEdit")
         layerRename.innerHTML = "&#128393;";
         tools.appendChild(layerRename);
-        layerRename.addEventListener('click', editLayer.bind(this, layer))
+        layerRename.addEventListener('click', editLayer.bind(null, layer, layerTitle))
+
+        // layer delete button
         const btnDeleteLayer = document.createElement("button");
         btnDeleteLayer.classList.add("btnDeleteLayer")
         btnDeleteLayer.innerHTML = "&Cross;";
         tools.appendChild(btnDeleteLayer);
+        btnDeleteLayer.addEventListener('click', deleteLayer.bind(this, layer, this.layers, layerBlock));
+
+        // layer show/hide shapes button
         const btnShowShapes = document.createElement("button");
         btnShowShapes.classList.add("btnShowShapes")
         tools.appendChild(btnShowShapes);
@@ -78,6 +77,7 @@ class LayerManager {
         const layerShapesBlock = document.createElement("div");
         layerShapesBlock.classList.add("layersManager_layer_shapes")
         layerBlock.appendChild(layerShapesBlock);
+        btnShowShapes.addEventListener('click', hideLayerShapes.bind(null, btnShowShapes, layerShapesBlock));
 
         for (let shape of layer.shapes) {
             layerShapesBlock.appendChild(this.createShape(shape, layer));
@@ -115,9 +115,31 @@ class LayerManager {
     }
 }
 
-function editLayer(layer) {
+function hideLayerShapes(btn, layerShapesBlock) {
+    layerShapesBlock.classList.toggle("hidden");
+    btn.classList.toggle("closed");
+}    
+
+function deleteLayer(layer, layers, layerBlock) {
+    if (confirm("Delete layer " + layer.name + "?")) {
+        layers.splice(layers.indexOf(layer), 1);
+        layerBlock.remove();
+    }
+    if (!layers.length) {
+        const layer = new Layer("Layer 1");
+        editLayer(layer);
+        this.createLayer(layer);
+    }
+}
+
+function editLayer(layer, layerTitle) {
     const newName = prompt("New layer name", layer.name);
-    if (newName) layer.name = newName;
+    if (newName) {
+        layer.name = newName;
+        if (layerTitle) {
+            layerTitle.innerText = newName;
+        }
+    }
 }
 
 function editShape(layer) {
@@ -146,27 +168,43 @@ function layersManager_shapeOver(shape, evt) {
     shape.backgroundColor = color;
 }
 
+function layerToggleVisible(layer, btn) {
+    layer.visible = !layer.visible;
+    if (layer.visible) {
+        btn.removeAttribute("hide")
+    } else {
+        btn.setAttribute("hide", "true")
+    }
+}
+
 
 function layersManagerMouseUp(evt) {
     const movingItem = this.movingItem;
     this.movingItem = undefined;
     if (!movingItem) return;
+    const movingDiv = movingItem.div;
 
     const layersManager = this.layersManagerDiv;
 
     if (movingItem) {
-        movingItem.div.classList.remove("moving");
+        movingDiv.classList.remove("moving");
 
         let overElem = evt.target;
         try {
-            const classToFind = movingItem.div.className;
+            const classToFind = movingDiv.className;
             let forceExit = false;
             while (!overElem.classList.contains(classToFind)
                 && overElem !== layersManager && !forceExit) {
-                
-                    if (classToFind === "layersManager_shapes_head") {
+
+                if (overElem === document.body) {
+                    forceExit = true;
+                    overElem = layersManager;
+                    break;
+                } else if (classToFind === "layersManager_shapes_head") {
                     if (overElem.className === "layersManager_layer_shapes") {
                         forceExit = true;
+                    } else {
+                        overElem = overElem.parentElement;
                     }
                 } else {
                     overElem = overElem.parentElement;
@@ -177,20 +215,48 @@ function layersManagerMouseUp(evt) {
             overElem = layersManager;
         }
         if (overElem === layersManager) {
-            if (movingItem.div.previousNextSibling) {
-                const previousNextSibling = movingItem.div.previousNextSibling;
-                previousNextSibling.parentElement.insertBefore(movingItem.div, previousNextSibling)
+            if (movingDiv.previousNextSibling) {
+                const previousNextSibling = movingDiv.previousNextSibling;
+                previousNextSibling.parentElement.insertBefore(movingDiv, previousNextSibling)
             } else {
-                movingItem.div.previousParent.appendChild(movingItem.div)
+                movingDiv.previousParent.appendChild(movingDiv)
             }
         } else if (overElem.classList.contains("layersManager_layer_shapes")) {
             if (!overElem.childElementCount) {
-                overElem.appendChild(movingItem.div)
+                overElem.appendChild(movingDiv)
             } else {
-                overElem.insertBefore(movingItem.div, overElem.firstElementChild)
+                overElem.insertBefore(movingDiv, overElem.firstElementChild)
             }
         } else {
-            overElem.parentElement.insertBefore(movingItem.div, overElem)
+            overElem.parentElement.insertBefore(movingDiv, overElem)
+        }
+        if (movingDiv.shape) { // is a shape
+            if (movingDiv.layer !== overElem.layer) {
+                movingDiv.layer.shapes.splice(movingDiv.layer.shapes.indexOf(movingDiv.shape), 1);
+                const overIndex = overElem.layer.shapes.indexOf(overElem.shape);
+                if (overIndex === -1) {
+                    overElem.layer.shapes.push(movingDiv.shape)
+                } else {
+                    overElem.layer.shapes.splice(overIndex, 0, movingDiv.shape)
+                }
+                console.log(this.layers);
+            } else {
+                // same layer, just reorder
+                if (movingDiv.shape !== overElem.shape) {
+                    movingDiv.layer.shapes.splice(movingDiv.layer.shapes.indexOf(movingDiv.shape), 1);
+                    const overIndex = movingDiv.layer.shapes.indexOf(overElem.shape);
+                    movingDiv.layer.shapes.splice(overIndex, 0, movingDiv.shape)
+                    console.log(this.layers);
+                }
+            }
+
+        } else { // is a layer
+            if (overElem.layer !== movingDiv.layer) {
+                this.layers.splice(this.layers.indexOf(movingDiv.layer), 1);
+                const overIndex = this.layers.indexOf(overElem.layer);
+                this.layers.splice(overIndex, 0, movingDiv.layer)
+                console.log(this.layers);
+            }
         }
     }
 }
