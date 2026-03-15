0abcf03be094023b0023cadacc02c38f7d01e94d
Jon Ander <jonander.echeverria@gmail.com>
2026-03-15 16:11:58 +0100

    pictureShips some docs, issues fix

--- public/js/canvas/gameClasses.js
+++ public/js/canvas/gameClasses.js
@@ -1,5 +1,6 @@
 import {
     Ellipse,
+    Picture,
     Polygon,
     Rect,
     Text
@@ -30,15 +31,15 @@ class Player {
         this.hide = false;
         this.isDead = false;
         this.scale = 1;
-        this.calculateScale();
         const debugEnabled = (typeof localStorage !== 'undefined') && localStorage.getItem('debug');
         if (debugEnabled) {
             console.log(`Player ${this.name} created with ship ${this.ship.name}`);
             console.log(this.ship);
         }
+        this.picture = null;
+        this.calculateScale();
     }
-
-    draw(context) {
+    draw_old(context) {
         if (this.hide) return;
         const rotationCenter = { x: this.ship.width / 2, y: this.ship.height / 2 };
         const layerOptions = {
@@ -56,6 +57,85 @@ class Player {
         this.nameShape.y = layerOptions.y - 20;
         this.nameShape.draw(context, { x: 0, y: 0 });
     }
+    /**
+     * Draws the player's ship on the canvas. If the ship is hidden, it returns without drawing. It calculates the real dimensions of the ship based on its scale and translation, and then calls drawPicture to render the ship using a pre-rendered picture for optimized performance. Finally, it draws the player's name above the ship.
+     * @param {CanvasRenderingContext2D} context 
+     */
+    draw(context) {
+
+        if (this.hide) return;
+        const realDimension = this.getRealDimension();
+        const rotationCenter = { x: realDimension.width / 2, y: realDimension.height / 2 };
+        const layerOptions = {
+            x: realDimension.x,
+            y: realDimension.y,
+            rotate: this.rotate,
+            rotationCenter,
+            scale: 1
+        };
+        
+        this.drawPicture(context, layerOptions);
+
+        this.nameShape.x = layerOptions.x;
+        this.nameShape.y = layerOptions.y - 20;
+        this.nameShape.draw(context, { x: 0, y: 0 });
+    }
+    /**
+     * Draws the player's ship using vector graphics. This method is less optimized than drawPicture, but allows for more dynamic rendering options such as scaling and rotation without needing to re-render a picture. It iterates through each layer of the ship and draws it according to the specified options.
+     * @param {CanvasRenderingContext2D} context 
+     */
+    drawVectorial(context) {
+        if (this.hide) return;
+        const rotationCenter = { x: this.ship.width / 2, y: this.ship.height / 2 };
+        const layerOptions = {
+            x: 0,
+            y: 0,
+            rotate: 0,
+            rotationCenter,
+            scale: this.scale
+        };
+
+        for (const layer of this.layers) {
+            layer.draw(context, layerOptions);
+        }
+        //this.nameShape.x = layerOptions.x;
+        //this.nameShape.y = layerOptions.y - 20;
+        //this.nameShape.draw(context, { x: 0, y: 0 });
+    }
+    /**
+     * Draws the player's ship using a pre-rendered picture for optimized performance. If the picture is not yet rendered, it calls the render method to create it before drawing.
+     * @param {CanvasRenderingContext2D} context 
+     * @param {*} layerOptions 
+     */
+    drawPicture(context, layerOptions) {
+        if (this.hide) return;
+        if (!this.picture) {
+            this.render();
+        }
+        this.picture.draw(context, layerOptions);
+    }
+    /**
+     * Renders the player's ship into a picture for optimized drawing. This is called whenever the player's scale changes, to ensure the picture is up to date with the current size.
+     */
+    render() {
+        console.log(`Rendering picture for player ${this.name}...`);
+        if (!this.pictureCanvas) {
+             this.pictureCanvas = document.createElement('canvas');
+        }
+        const realDimension = this.getRealDimension();
+        this.pictureCanvas.width = realDimension.width;
+        this.pictureCanvas.height = realDimension.height;
+        // reset canvas
+        const offscreenCanvas = this.pictureCanvas;
+        const offscreenContext = offscreenCanvas.getContext('2d');
+        offscreenContext.clearRect(0, 0, offscreenCanvas.width, offscreenCanvas.height);
+        this.drawVectorial(offscreenContext);
+        this.picture = new Picture(offscreenCanvas,null, 0, 0, realDimension.width, realDimension.height, 0, 0, realDimension.width, realDimension.height);
+    }
+    /**
+     * Calculates the scale of the player's ship based on a standard size and the player's kills and deaths. It adjusts the real width and height of the ship accordingly, as well as the translation needed to keep the ship centered. Finally, it calls render to update the picture with the new scale.
+     * @param {number} sizeStandard 
+     */
     calculateScale(sizeStandard = 100) {
         let scaleDec = 1;
         if (sizeStandard) {
@@ -72,7 +152,12 @@ class Player {
         this.xTranslation = (this.width - this.realWidth) / 2;
         this.yTranslation = (this.height - this.realHeight) / 2;
         this.scale = scaleDec;
+        this.render();
     }
+    /**
+     * Returns the real dimensions of the player's ship, including the x and y position adjusted for translation, and the real width and height based on the current scale.
+     * @returns {Object} An object containing the real dimensions of the player's ship, including the x and y position adjusted for translation, and the real width and height based on the current scale. This is used for accurate collision detection and drawing calculations.
+     */
     getRealDimension() {
         return {
             x: this.x + this.xTranslation,
@@ -81,9 +166,13 @@ class Player {
             height: this.realHeight
         }
     }
+    /**
+     * Creates a new bullet object based on the player's current position, rotation, and speed. The bullet is initialized with the player's socket ID, starting position at the center of the ship, angle of rotation, and speed. The bullet is then added to the player's bullets array and returned for further processing (such as adding to the game world or sending to the server).
+     */
     createBullet() {
-        let bPosX = this.x + this.width / 2;
-        let bPosY = this.y + this.height / 2;
+        const realDimension = this.getRealDimension();
+        let bPosX = realDimension.x + realDimension.width / 2;
+        let bPosY = realDimension.y + realDimension.height / 2;
         const bullet = new Bullet(this.socketId, bPosX, bPosY, this.rotate, this.speed, this.rotate);
         this.bullets.push(bullet);
         return bullet;
@@ -108,7 +197,6 @@ class Player {
             scale: this.scale,
             xTranslation: this.xTranslation,
             yTranslation: this.yTranslation
-            //bullets: this.bullets.map(bullet => bullet.getSortDetails())
         }
     }
     getDistanceToPlayer(player) {
